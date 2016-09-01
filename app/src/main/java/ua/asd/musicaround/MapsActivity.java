abd362ebed004995ad759932e347f81f51f4f1d0
Nick <lanikolya1989@gmail.com>
2016-09-01 21:18:26 +0300

    maps

---
+++ app/src/main/java/ua/asd/musicaround/MapsActivity.java
@@ -0,0 +1,34 @@
+package ua.asd.musicaround;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Add a marker in Dnipro and move the camera
+        LatLng dnipro = new LatLng(48.45810997, 35.00414461);
+        mMap.addMarker(new MarkerOptions().position(dnipro).title("Marker in Dnipro"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(dnipro));
+    }
+}
